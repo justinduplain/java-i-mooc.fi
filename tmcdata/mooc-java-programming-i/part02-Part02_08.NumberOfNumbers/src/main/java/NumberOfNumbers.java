0dfc482a7f36f4acd5ece548b5f6a8c1a3398464
pmentropy <justin.duplain@gmail.com>
2022-12-12 20:23:31 -0500

    through 02_12

--- tmcdata/mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ tmcdata/mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,19 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum = 0;
 
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+        
+            if (number == 0) {
+                break;
+            }
+ 
+            sum+=number
+        }
+ 
+        System.out.println("Sum of number " + sum);
     }
 }
