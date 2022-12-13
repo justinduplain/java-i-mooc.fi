ab73108b090051653b06ed0b35dce5929c470739
pmentropy <justin.duplain@gmail.com>
2022-12-12 22:28:31 -0500

    through 2-19

--- tmcdata/mooc-java-programming-i/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ tmcdata/mooc-java-programming-i/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -5,6 +5,14 @@ public class CountingToHundred {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+ 
+        //System.out.println("Give a number: ");
+        int input = Integer.valueOf(scanner.nextLine());
+ 
+        for(int i = input; i <= 100; i++){
+            System.out.println(i);
+        }
+ 
     }
 }
+ 
