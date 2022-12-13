cd6ee97ba190d5cda24bfe7ad118d72343bf0947
pmentropy <justin.duplain@gmail.com>
2022-12-12 22:14:49 -0500

    through 02-14

--- tmcdata/mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ tmcdata/mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,28 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double sum = 0.0;
+        int count = 0;
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number < 0) {
+                continue;
+            }
+            if (number == 0) {
+                break;
+            }
+            if (number > 0) {
+                sum += number;
+                count++;
+            }
 
+        }
+        if (count == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println(sum/count);
+        }
     }
 }
