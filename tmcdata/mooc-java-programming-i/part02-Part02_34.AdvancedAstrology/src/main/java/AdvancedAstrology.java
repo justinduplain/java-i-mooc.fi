087b416649b571d53defaeba2efc721830688150
pmentropy <justin.duplain@gmail.com>
2022-12-18 21:56:51 -0500

    through section 02

--- tmcdata/mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ tmcdata/mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -2,19 +2,34 @@
 public class AdvancedAstrology {
 
     public static void printStars(int number) {
-        // part 1 of the exercise
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
     }
 
     public static void printSpaces(int number) {
-        // part 1 of the exercise
+        for (int i = 0; i < number; i++) {
+            System.out.print(" ");
+        }
     }
 
     public static void printTriangle(int size) {
-        // part 2 of the exercise
+        int j = size - 1;
+        for (int i = 1; i <= size; i++) {
+            printSpaces(j);
+            printStars(i);
+            j--;
+        }
     }
 
     public static void christmasTree(int height) {
-        // part 3 of the exercise
+        printTriangle(height);
+        printSpaces(height - 2);
+        printStars(3);
+        printSpaces(height - 2);
+        printStars(3);
+
     }
 
     public static void main(String[] args) {
